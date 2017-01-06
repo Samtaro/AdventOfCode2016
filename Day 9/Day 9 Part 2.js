9d41b4a7b8bb9563a0f42d9dfc74e0e60b474cb4
Sam Wong <samtarowong@gmail.com>
2017-01-05 19:00:49 -0500

    Commiting Day 9 Part 2 to work on Desktop

---
+++ Day 9/Day 9 Part 2.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const puzzleToArray = fs.readFileSync('d9puzzle.txt').toString();
+
+(function decompressedLength(puzzle){
+  var decompressedString = "";
+  var counter = 0;
+  while(counter < puzzle.length){
+    if(puzzle.charAt(counter) === "(" ){
+      var marker = puzzle.substring(counter, puzzle.indexOf(")", counter)+1)
+      var numberOfLetters = parseInt(marker.substring(1, marker.indexOf("x")))
+      var repeatTimes = parseInt(marker.substring(marker.indexOf("x")+1, marker.length-1))
+      var repeatedString = puzzle.substr(puzzle.indexOf(")", counter)+1, numberOfLetters);
+      var repeating = decompressedLength(repeatedString)
+
+      for(var i = 0; i<repeatTimes; i++){
+        decompressedString += repeating
+        }
+      counter = puzzle.indexOf(")", counter) + numberOfLetters +1
+    }
+    else{
+      decompressedString += puzzle.charAt(counter)
+      counter++
+    }
+  }
+  console.log(decompressedString.trim().length)
+  return decompressedString.trim()
+})(puzzleToArray)
